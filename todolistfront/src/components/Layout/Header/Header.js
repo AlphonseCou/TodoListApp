06c78fc7b7606e11ddf07b288893fb965e42b685
Alphonse <alphonsecouturier@gmail.com>
2022-09-13 23:32:23 +0100

    Correction in setting backend & start css in frontend on header

--- todolistfront/src/components/Layout/Header/Header.js
+++ todolistfront/src/components/Layout/Header/Header.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+
 import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
-      <Link to="/">Accueil</Link>
-      <div className="header_center">
-        <Link to="/todolist">Afficher la liste de taches</Link>
+      <div className="div-title">
+        <h1 className="title">Todo List Application</h1>
       </div>
+      <Link to="/">
+        <Button className="header-button">Accueil</Button>
+      </Link>
+      <Link to="/todolist">
+        <Button className="header-button">Afficher la liste de taches</Button>
+      </Link>
     </div>
   );
 }
