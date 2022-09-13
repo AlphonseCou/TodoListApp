b2b9086ad1ecbe25ab78f5b0820191ebffdcefb9
Alphonse <alphonsecouturier@gmail.com>
2022-09-13 18:44:50 +0100

    Add axios in frontend and create the first request get for a todo & add django-cors-header for the backend

--- todolistfront/src/pages/TodoList/TodoList.js
+++ todolistfront/src/pages/TodoList/TodoList.js
@@ -1,25 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import ToggleButton from "react-bootstrap/ToggleButton";
 import "./TodoList.css";
 
 function TodoList() {
   const [checked, setChecked] = useState(false);
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    async function getAllTodo() {
+      try {
+        const todos = await axios.get("http://localhost:8080/todos/todo/");
+        setTodos(todos.data);
+      } catch (error) {}
+    }
+    getAllTodo();
+  }, []);
   return (
     <>
       <div className="todolist">
-        <li>
-          <ToggleButton
-            className="mb-2"
-            id="toggle-check"
-            type="checkbox"
-            variant="outline-primary"
-            checked={checked}
-            value="1"
-            onChange={(e) => setChecked(e.currentTarget.checked)}
-          >
-            Fait
-          </ToggleButton>
-        </li>
+        {todos.map((todo, i) => {
+          return (
+            <li>
+              {todo.name}
+              <ToggleButton
+                className="mb-2"
+                id="toggle-check"
+                type="checkbox"
+                variant="outline-primary"
+                checked={checked}
+                value="1"
+                onChange={(e) => setChecked(e.currentTarget.checked)}
+              >
+                Fait
+              </ToggleButton>
+            </li>
+          );
+        })}
       </div>
     </>
   );
