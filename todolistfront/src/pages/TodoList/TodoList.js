b2e057d95d03d60e4b2d444ce005fb4a619f9a61
Alphonse <alphonsecouturier@gmail.com>
2022-09-14 11:38:20 +0100

    add todo in DB and display todo list with name and status

--- todolistfront/src/pages/TodoList/TodoList.js
+++ todolistfront/src/pages/TodoList/TodoList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ToggleButton from "react-bootstrap/ToggleButton";
+
 import "./TodoList.css";
 
 function TodoList() {
@@ -18,25 +19,29 @@ function TodoList() {
   }, []);
   return (
     <>
-      <div className="todolist">
-        {todos.map((todo, i) => {
-          return (
-            <li>
-              {todo.name}
-              <ToggleButton
-                className="mb-2"
-                id="toggle-check"
-                type="checkbox"
-                variant="outline-primary"
-                checked={checked}
-                value="1"
-                onChange={(e) => setChecked(e.currentTarget.checked)}
-              >
-                Fait
-              </ToggleButton>
-            </li>
-          );
-        })}
+      <div className="page_todolist">
+        <div className="div_todolist">
+          {todos.map((todo, i) => {
+            return (
+              <li className="li_todolist" key={todo.name}>
+                {todo.name}
+                <div className="status_todo">
+                  <ToggleButton
+                    className="togglebutton_todo"
+                    id="toggle-check"
+                    type="checkbox"
+                    size="xlg"
+                    checked={todo.status}
+                    value="1"
+                    onChange={(e) => setChecked(e.currentTarget.checked)}
+                  >
+                    Fait
+                  </ToggleButton>
+                </div>
+              </li>
+            );
+          })}
+        </div>
       </div>
     </>
   );
